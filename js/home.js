8fbc06645d89cb59729b905fb2717dacb6e0c3eb
pasuth73 <pasuth73@hotmail.com>
2016-04-10 21:23:02 +0700

    Create function Event in home.html,home.js

--- js/home.js
+++ js/home.js
@@ -11,6 +11,33 @@ $(document).ready(function() {
         $("#amount-today-sammary").text(json.amount_today_sammary);
     });
 
+    $.post("/event", { quantity: 3 }, function(data) {
+        var json = JSON.parse(data);
+        $.each(json, function(i, item) {
+            switch (item.type) {
+                case "information":
+                    $("event-con").append('<div class="uk-panel uk-panel-box">'
+                        + '<div class="uk-panel-badge uk-badge">INFORMATION</div>'
+                        + '<h3 class="uk-panel-title">' + item.title + '</h3>' + item.msg
+                        + '</div>');
+                    break;
+                case "warning":
+                    $("event-con").append('<div class="uk-panel uk-panel-box">'
+                        + '<div class="uk-panel-badge uk-badge uk-badge-warning">WARNING</div>'
+                        + '<h3 class="uk-panel-title">' + item.title + '</h3>' + item.msg
+                        + '</div>');
+                    break;
+                case "danger":
+                    $("event-con").append('<div class="uk-panel uk-panel-box">'
+                        + '<div class="uk-panel-badge uk-badge">DANGER</div>'
+                        + '<h3 class="uk-panel-title">' + item.title + '</h3>' + item.msg
+                        + '</div>');
+                    break;
+            }
+        });
+
+    });
+
     $.post("/sumary", { date: "curent-month" }, function(data) {
         var json = JSON.parse(data);
         $("#wh-month-sammary").text(json.wh_month_sammary);
